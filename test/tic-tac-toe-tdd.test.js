89fac19ff255dad04d267dfeb0301d5f8b5b822b
Vee Uye <vee@accommodationforstudents.com>
2022-08-10 21:39:03 +0100

    initial commit

---
+++ test/tic-tac-toe-tdd.test.js
@@ -0,0 +1,12 @@
+let playerO
+let playerX
+
+const game = { players: [playerO, playerX] }
+
+describe('tic-tac-toe', function () {
+    it('should have two players', () => {
+        const players = game.players
+
+        expect(players.length).toEqual(2)
+    })
+})
