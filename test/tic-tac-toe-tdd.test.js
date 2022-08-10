6990aff520c30addf5fc37f9184f3141dc9c51de
Vee Uye <vee@accommodationforstudents.com>
2022-08-10 22:35:56 +0100

    chore: update linter

--- test/tic-tac-toe-tdd.test.js
+++ test/tic-tac-toe-tdd.test.js
@@ -4,9 +4,9 @@ let playerX
 const game = { players: [playerO, playerX] }
 
 describe('tic-tac-toe', function () {
-    it('should have two players', () => {
-        const players = game.players
+  it('should have two players', () => {
+    const players = game.players
 
-        expect(players.length).toEqual(2)
-    })
+    expect(players.length).toEqual(2)
+  })
 })
